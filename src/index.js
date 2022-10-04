583c5ef2b9bec2119298171ab03b27d3ead23b7c
Niusha <35556570+NiushaH@users.noreply.github.com>
2022-10-04 09:07:45 -0700

    Complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+  //grab all the necessary DOM elements
+
   // your code here
+  const submitBox = document.querySelector("#new-task-description");
+
+  submitBox.addEventListener("click", submitBoxClick, false);
+  
+  function submitBoxClick(event) {
+    let warn = "preventDefault() won't let you submit this form!<br>";
+    document.getElementById("create-task-form").innerHTML += warn;
+    event.presentDefault();
+  }; 
 });
